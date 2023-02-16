58ec23231ffd43fdb95caf1dc391b608d8943e35
M.Raffi <mraffi1778@gmail.com>
2023-02-16 16:14:22 +0700

    deplyoment

---
+++ README.md
@@ -0,0 +1,4 @@
+Nama: M.Raffi
+NPM :2106653760 
+kelas : Z
+Hobi : Tidur
---
+++ money_tracker/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from money_tracker.models import TransactionRecord
+
+def show_tracker(request):
+    transaction_data = TransactionRecord.objects.all()
+    context = {
+        'list_of_transactions': transaction_data,
+        'name': 'M.Raffi'
+    }
+
+    return render(request, "tracker.html", context)
+    
+
