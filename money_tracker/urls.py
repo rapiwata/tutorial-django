5feca859d4c871c134fb82d711b2d1636c686643
M.Raffi <mraffi1778@gmail.com>
2023-02-22 14:10:41 +0700

    mengumpulkan tutorial 2

--- money_tracker/urls.py
+++ money_tracker/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from money_tracker.views import show_tracker
 from money_tracker.views import create_transaction
 from money_tracker.views import show_xml 
+from money_tracker.views import show_json 
+from money_tracker.views import show_xml_by_id, show_json_by_id
+
+
 
 
 app_name = 'money_tracker'
@@ -10,6 +14,10 @@ urlpatterns = [
     path('', show_tracker, name='show_tracker'),
     path('xml/', show_xml, name='show_xml'),
     path('create', create_transaction, name='create_transaction'),
+    path('json/', show_json, name='show_json'), 
+    path('xml/<int:id>', show_xml_by_id, name='show_xml_by_id'),
+    path('json/<int:id>', show_json_by_id, name='show_json_by_id'), 
+
 
 
 ]
--- money_tracker/views.py
+++ money_tracker/views.py
@@ -32,6 +32,28 @@ def show_xml(request):
 
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 
+def show_json(request):
+    data = TransactionRecord.objects.all()
+
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request, id):
+    data = TransactionRecord.objects.filter(pk=id)
+
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json_by_id(request,id):
+    data = TransactionRecord.objects.filter(pk=id)
+
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+
+
+
+
+
+
+
 
 
 
