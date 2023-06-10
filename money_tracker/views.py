38fb25e6dd1282d6249d662a759b042d1f8805f7
M.Raffi <mraffi1778@gmail.com>
2023-06-10 12:08:14 +0700

    deploy

--- money_tracker/views.py
+++ money_tracker/views.py
@@ -1,3 +1,4 @@
+import json
 from django.shortcuts import render
 from money_tracker.models import TransactionRecord
 from django.http import HttpResponseRedirect
@@ -16,6 +17,7 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import logout as auth_logout
 
 
 
