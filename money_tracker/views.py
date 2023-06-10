a3a2982f8e287542e8a6ca835075153179251a4c
M.Raffi <mraffi1778@gmail.com>
2023-06-10 12:27:16 +0700

    d

--- money_tracker/views.py
+++ money_tracker/views.py
@@ -17,7 +17,7 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from django.contrib.auth import logout as auth_logout
+
 
 
 
